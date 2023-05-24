804b39160024024aa5b8037477d04c838accd4ba
ista1024 <ista1024@gmail.com>
2023-05-24 19:51:53 +1000

    fixed api url

--- services/article.ts
+++ services/article.ts
@@ -3,20 +3,19 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const articleApi = createApi({
   reducerPath: 'articleApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+    baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
   }),
   endpoints: (builder) => ({
     getSummary: builder.query({
       query: (params) => {
         return {
-          url: `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+          url: `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
           headers: {
             'X-RapidAPI-Key': params.rapidApiKey,
             'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
           }
         }
       },
-
     })
   }),
 })
