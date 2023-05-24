91f94142c8ffa60f22db462409d5711624780b0d
ista1024 <ista1024@gmail.com>
2023-05-24 14:39:44 +1000

    wrap RootLayout with redux provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
+"use client";
+import { Provider } from 'react-redux'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { store } from '@/services/store'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+      <body className={inter.className}>
+        <Provider store={store}>
+          {children}
+        </Provider>
+      </body>
+    </html >
   )
 }
